732b0eee568990d627fc05c011a64da3bed681d1
LoznyX <yunogasaiprivate@gmail.com>
2026-09-25 10:56:12 +0200

    kartkowkazad

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+      /* 1. wylosuj 100 liczb z zakresu od 1 do 50
+      2. zapisz liczby w liscie
+      3.policz ile jest wartosci unikatowych
+      4.zapisz liste z wartosciami tylko parzystymi
+      5.oblicz ile jest liczb 1 wylosowanych
+      6.zrob klase ksiazka z polem tytul autor czy dostepna
+      7.zrob klase czytelnik z polem imie i wyporzyczone ksiazki
+      8.zrob klase biblioteka z polami ksiazki,czytelnicy
+      9.zaprogramuj wszystko co potrzebujesz zeby zrobic meode wyporzycz,
+      ksziazka po wyporzyczeniu ksziazka ma byc niedostepna do wyporzyczenia a uzytkownik
+      ktory ma ja wyporzyczano wyswietlac sie bedzie u niego
+
+       */
+        }
+
+}
